a64261637bd02601acdf2ce93a8eab2d1331c1e1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-01-30 17:47:49 +0100

    initial Commit

---
+++ scrimba-projects/src/components/About.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className='about--header'>About</h1>
+      <p className='about--content'>
+        I am a FrontEnd Developer with particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn
+      </p>
+    </div>
+  );
+}
+
+export default About;
