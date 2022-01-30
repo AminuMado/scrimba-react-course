1348772de72d27b7d1409f0b61c18d5f783f5d37
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-01-30 17:48:57 +0100

    inital commit

---
+++ scrimba-projects/src/components/Intrest.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Interest() {
+  return (
+    <div>
+      <h2 className='interest--header'>Interest</h2>
+      <p className='interest--content'>
+        Food Expert, Music Scholar,Reader, Internet Fanatic,Entrepreneur,Travel
+        Geek,Pop Culture Ninja,Have Strong Opinions about Pizzas
+      </p>
+    </div>
+  );
+}
+
+export default Interest;
